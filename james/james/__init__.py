33bb9228a6883bc33d40babc4655807700a4d585
Felipe Godoi Rosario <felipe.rosario@gmail.com>
2018-01-02 18:04:14 +0000

    Added more security to Production environment configuration.

--- james/james/__init__.py
+++ james/james/__init__.py
@@ -1,9 +1,24 @@
+import os
+
 from pyramid.config import Configurator
+
+
+def get_from_env_or_settings(settings_name, settings, env_name=None):
+    if not env_name:
+        env_name = settings_name.upper().replace('.', '_')
+
+    return os.environ.get(env_name, settings.get(settings_name))
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    settings['sqlalchemy.url'] = get_from_env_or_settings('sqlalchemy.url',
+                                                          settings)
+    settings['auth.secret'] = get_from_env_or_settings('auth.secret',
+                                                       settings)
+    settings['api.key'] = get_from_env_or_settings('api.key', settings)
+    
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
